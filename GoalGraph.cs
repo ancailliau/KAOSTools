c0bb18600e1b69e66f48680285aa21131cc77e2d
Antoine Cailliau <antoine.cailliau@uclouvain.be>
2011-05-09 10:15:26 +0200

    Implements display of rectangles

--- GoalGraph.cs
+++ GoalGraph.cs
@@ -1,5 +1,7 @@
 using System;
 using Cairo;
+using System.Collections.Generic;
+using Shapes;
 
 namespace Editor
 {
@@ -10,9 +12,20 @@ namespace Editor
 			set;
 		}
 		
+		public List<RectangleShape> Rectangles {
+			get;
+			set;
+		}
+		
 		public GoalGraph ()
 		{
 			BackgroundColor = new Color(1, 1, 1);
+			Rectangles = new List<RectangleShape>();
+			Rectangles.Add(new RectangleShape() {
+				TopLeft = new PointD(100, 100),
+				Width = 100,
+				Height = 40
+			});
 		}
 		
 		protected override bool OnExposeEvent (Gdk.EventExpose evnt)
@@ -24,6 +37,19 @@ namespace Editor
 			using (Context context = Gdk.CairoHelper.Create(evnt.Window)) {
 				PaintBackground(context);
 				
+				// Y-Axis is inverted 
+				context.Transform(new Matrix(1,0,0,-1,0,0));
+				
+				// Re-adjust center
+				context.Translate(0, -height);
+				
+				// For crisp lines
+				context.Translate(0.5, 0.5);				
+				context.LineWidth = 1;				
+				
+				foreach (var rect in Rectangles) {
+					rect.Display(context);
+				}
 			}
 			return true;
 		}
