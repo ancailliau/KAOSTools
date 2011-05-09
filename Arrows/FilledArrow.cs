16802db0b67d39079d1a7fda32875398b4bd12d1
Antoine Cailliau <antoine.cailliau@uclouvain.be>
2011-05-09 15:10:56 +0200

    Add missing files

---
+++ Arrows/FilledArrow.cs
@@ -0,0 +1,75 @@
+using System;
+using Shapes;
+using Cairo;
+using Gtk;
+
+namespace Arrows
+{
+	public class FilledArrow
+	{
+		
+		public IShape Start {
+			get;
+			set;
+		}
+		
+		public IShape End {
+			get;
+			set;
+		}
+		
+		public Color StrokeColor {
+			get;
+			set;
+		}
+		
+		public Color FillColor {
+			get;
+			set;
+		}
+		
+		private double arrowHalfAngle;
+		private double arrowSideWith;
+		
+		public FilledArrow ()
+		{
+			arrowHalfAngle = Math.Atan2(5, 10);
+			arrowSideWith = Math.Sqrt(125);
+		}
+		
+		public void Display (Context context, DrawingArea drawingArea)
+		{
+			var startPosition = Start.GetAnchor(End.Position);
+			var endPosition = End.GetAnchor(Start.Position);
+			
+			var oldSource = context.Source;
+			
+			context.SetSourceRGB(StrokeColor.R,
+				StrokeColor.G,
+				StrokeColor.B);
+			
+			context.MoveTo(startPosition);
+			context.LineTo(endPosition);
+			
+			context.Stroke();
+			
+			context.MoveTo(endPosition);
+			
+			double xx = startPosition.X - endPosition.X;
+			double yy = startPosition.Y - endPosition.Y;
+			double alpha = Math.Atan2(yy, xx);
+			
+			double gamma = alpha - arrowHalfAngle;
+			context.LineTo(endPosition.X + arrowSideWith * Math.Cos(gamma), endPosition.Y + arrowSideWith * Math.Sin(gamma));
+			
+			double gamma2 = alpha + arrowHalfAngle;
+			context.LineTo(endPosition.X + arrowSideWith * Math.Cos(gamma2), endPosition.Y + arrowSideWith * Math.Sin(gamma2));
+			
+			context.Fill();
+			
+			context.Source = oldSource;
+		}
+		
+	}
+}
+
