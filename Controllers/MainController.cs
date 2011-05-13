9d35c64d46b7ee4b6d520e2940207edb39cc24fc
Antoine Cailliau <antoine.cailliau@uclouvain.be>
2011-05-13 15:27:28 +0200

    Refactors and improve code structure

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using Model;
 using Editor.Windows;
 using Editor.Model;
+using Gtk;
 
 namespace Editor.Controllers
 {
 	public class MainController
 	{
+		private string filename = "";
 		
 		public GoalModel Model {
 			get;
@@ -33,6 +35,66 @@ namespace Editor.Controllers
 		{
 			Window.Present();
 		}
+		
+		public void Quit ()
+		{
+			Application.Quit();
+		}
+		
+		public void Save ()
+		{
+			if (this.filename == "") {
+				this.SaveAs ();
+				
+			} else {
+				new XmlExporter(this.filename, Model, Views).Export();
+			}
+		}
+		
+		public void SaveAs ()
+		{
+			var dialog = new FileChooserDialog("Save file", this.window,
+			FileChooserAction.Save, "Cancel", ResponseType.Cancel, "Save", ResponseType.Accept);
+			
+			if (dialog.Run() == (int) ResponseType.Accept) {
+				this.filename = dialog.Filename;
+				Save();
+			}
+			
+			dialog.Destroy();		
+		}
+		
+		public void Load ()
+		{
+			var dialog = new FileChooserDialog ("Choose file to open...",
+			this.window, FileChooserAction.Open, "Cancel", ResponseType.Cancel, "Open", ResponseType.Accept);
+			
+			if (dialog.Run() == (int) ResponseType.Accept) {
+				this.filename = dialog.Filename;
+				var importer = new XmlImporter(dialog.Filename);
+				importer.Import();
+				
+				this.Model = importer.Model;
+				this.Views = importer.Views;
+				
+				if (this.Views.Count > 0) {
+					foreach (var v in importer.Views) {
+						Views.Add(v);
+					}
+					
+				} else {
+					var errorDialog = new MessageDialog(this.window, DialogFlags.DestroyWithParent, MessageType.Error,
+						ButtonsType.Ok, false, "File is malformed.");
+					
+					if (errorDialog.Run() > 0) {
+						errorDialog.Destroy();
+					}
+				}
+			}
+			
+			dialog.Destroy();
+		}
+		
 	}
 }
 
--- Windows/MainWindow.cs
+++ Windows/MainWindow.cs
@@ -72,77 +72,28 @@ public partial class MainWindow: Gtk.Window
 	
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
-		Application.Quit ();
+		this.Controller.Quit ();
 		a.RetVal = true;
 	}
 	
 	protected virtual void OnQuitActionActivated (object sender, System.EventArgs e)
 	{
-		Application.Quit();
+		this.Controller.Quit ();
 	}
 		
 	protected virtual void OnSaveActionActivated (object sender, System.EventArgs e)
 	{
-		if (this.filename == null | this.filename == "") {
-			SaveAs();
-		} else {
-			Save();
-		}
-	}
-	
-	protected void Save ()
-	{
-		new XmlExporter(this.filename, Model, Views).Export();
+		this.Controller.Save ();
 	}
 	
 	protected virtual void OnOpenActionActivated (object sender, System.EventArgs e)
 	{
-		
-		var dialog = new FileChooserDialog ("Choose file to open...",
-			this, FileChooserAction.Open, "Cancel", ResponseType.Cancel, "Open", ResponseType.Accept);
-		
-		if (dialog.Run() == (int) ResponseType.Accept) {
-			this.filename = dialog.Filename;
-			var importer = new XmlImporter(dialog.Filename);
-			importer.Import();
-			
-			this.Model = importer.Model;
-			this.Views = importer.Views;
-			
-			if (this.Views.Count > 0) {
-				foreach (var v in importer.Views) {
-					Views.Add(v);
-				}
-				
-			} else {
-				var errorDialog = new MessageDialog(this, DialogFlags.DestroyWithParent, MessageType.Error,
-					ButtonsType.Ok, false, "File is malformed.");
-				
-				if (errorDialog.Run() > 0) {
-					errorDialog.Destroy();
-				}
-			}
-		}
-		
-		dialog.Destroy();
+		this.Controller.Load ();
 	}
 	
 	protected virtual void OnSaveAsActionActivated (object sender, System.EventArgs e)
 	{
-		SaveAs();
+		this.Controller.SaveAs ();
 	}
-	
-	protected void SaveAs() {
-		var dialog = new FileChooserDialog("Save file", this,
-			FileChooserAction.Save, "Cancel", ResponseType.Cancel, "Save", ResponseType.Accept);
-		
-		if (dialog.Run() == (int) ResponseType.Accept) {
-			this.filename = dialog.Filename;
-			Save();
-		}
-		
-		dialog.Destroy();		
-	}
-	
 }
 }
