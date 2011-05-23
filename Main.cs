9da7c490954ab661f56b3ef932b4e5f8ee612c56
Antoine Cailliau <antoine.cailliau@uclouvain.be>
2011-05-23 14:59:04 +0200

    Implements global exception handling

--- Main.cs
+++ Main.cs
@@ -2,23 +2,47 @@ using Gtk;
 using KaosEditor.Controllers;
 using KaosEditor.Model;
 using KaosEditor.UI.Windows;
+using System;
+using GLib;
 
 namespace KaosEditor
 {
 	class MainClass
 	{
+		private static EditorModel model;
+		private static MainController controller;
+		private static MainWindow window;
+		
 		public static void Main (string[] args)
 		{
 			Application.Init ();
 			
-			var model = new EditorModel ();
-			var controller = new MainController(model);
-			var window = new MainWindow (model, controller);
+			model = new EditorModel ();
+			controller = new MainController(model);
+			window = new MainWindow (model, controller);
 			controller.Window = window;
 			
 			controller.Show ();
 			
+			ExceptionManager.UnhandledException += HandleExceptionManagerUnhandledException;
+			
 			Application.Run ();
+			
+		}
+
+		static void HandleExceptionManagerUnhandledException (UnhandledExceptionArgs args)
+		{
+			
+			var dialog = new MessageDialog (window,
+			DialogFlags.DestroyWithParent, MessageType.Error,
+			ButtonsType.Ok, false, args.ExceptionObject.ToString ());
+		
+			dialog.Response += delegate(object o, ResponseArgs args2) {
+				dialog.Destroy ();
+				// args.ExitApplication = true;
+			};
+			
+			dialog.Present ();
 		}
 		
 	}
