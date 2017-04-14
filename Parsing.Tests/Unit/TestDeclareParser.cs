2b03861a62452ab66bd3acbe797dba5574288458
Antoine Cailliau <antoine.cailliau@uclouvain.be>
2017-04-14 11:48:58 +0200

    Improves test TestFailWhenUnknownAttribute

--- Parsing.Tests/Unit/TestDeclareParser.cs
+++ Parsing.Tests/Unit/TestDeclareParser.cs
@@ -13,11 +13,13 @@ namespace UCLouvain.KAOSTools.Parsing.Tests.Unit
         [Test()]
         public void TestFailWhenUnknownAttribute() 
         {
+            var dg = new GoalDeclareParser();
             var e = Assert.Catch(() => {
-                var dg = new GoalDeclareParser();
-                dg.ParsedAttribute("attribute", null, null);
+                dg.ParsedAttribute("my_attribute", null, null);
             });
             Assert.IsInstanceOf(typeof(NotImplementedException), e);
+            Assert.That(e.Message.Contains("my_attribute"));
+            Assert.That(e.Message.Contains(dg.GetIdentifier()));
         }
     }
 }
