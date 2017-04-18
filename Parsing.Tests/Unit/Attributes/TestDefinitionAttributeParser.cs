9a8bef67229107b96f675ca1c7df8997c0dbfdda
Antoine Cailliau <antoine.cailliau@uclouvain.be>
2017-04-18 15:39:22 +0200

    Adds tests for DefinitionAttributeParser

---
+++ Parsing.Tests/Unit/Attributes/TestDefinitionAttributeParser.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using KAOSTools.Parsing.Parsers;
+using KAOSTools.Parsing.Parsers.Attributes;
+using NUnit.Framework;
+using UCLouvain.KAOSTools.Parsing.Parsers.Exceptions;
+using System.Linq;
+
+namespace UCLouvain.KAOSTools.Parsing.Tests.Unit.Attributes
+{
+    [TestFixture]
+    public class TestDefinitionAttributeParser
+    {
+        [TestCase ("")]
+        [TestCase ("Description")]
+        [TestCase ("   ")]
+        public void TestValidValue (string r)
+        {
+            var ap = new DefinitionAttributeParser ();
+            const string attIdentifier = "definition";
+
+            var e = ap.ParsedAttribute (attIdentifier, 
+                                        null,
+                                        new NParsedAttributeAtomic (new ParsedString (r)));
+            
+            Assert.IsInstanceOf (typeof (ParsedDefinitionAttribute), e);
+            var pa = (ParsedDefinitionAttribute)e;
+            Assert.AreEqual (r, pa.Value.Value);
+        }
+
+        [Test ()]
+        public void TestNotStringValue ()
+        {
+            var ap = new DefinitionAttributeParser ();
+            const string attIdentifier = "definition";
+
+            var e = Assert.Catch (() => {
+                ap.ParsedAttribute (attIdentifier,
+                                   null,
+                                   new NParsedAttributeAtomic (new ParsedFloat ()));
+            });
+
+            Assert.IsInstanceOf (typeof (InvalidAttributeValueException), e);
+            StringAssert.AreEqualIgnoringCase (
+                string.Format (InvalidAttributeValueException.STRING, attIdentifier),
+                e.Message
+            );
+        }
+
+        [Test]
+        public void TestNullParameter ()
+        {
+            var ap = new DefinitionAttributeParser ();
+            const string attIdentifier = "definition";
+
+            var e = Assert.Catch(() => {
+                ap.ParsedAttribute(attIdentifier,
+                                   new NParsedAttributeAtomic(new ParsedFloat()), 
+                                   new NParsedAttributeAtomic(new IdentifierExpression("software")));
+            });
+
+            Assert.IsInstanceOf(typeof(InvalidParameterAttributeException), e);
+            StringAssert.AreEqualIgnoringCase(
+                string.Format(InvalidParameterAttributeException.NO_PARAM, attIdentifier),
+                e.Message
+            );
+        }
+
+        [Test]
+        public void TestColonValue ()
+        {
+            TestNotColonValue (new NParsedAttributeColon ());
+        }
+
+        [Test]
+        public void TestBracketValue ()
+        {
+            TestNotColonValue (new NParsedAttributeBracket ());
+        }
+
+        [Test]
+        public void TestListValue ()
+        {
+            TestNotColonValue (new NParsedAttributeList (Enumerable.Empty<ParsedElement> ()));
+        }
+
+        public void TestNotColonValue (NParsedAttributeValue v)
+        {
+            var ap = new DefinitionAttributeParser ();
+            const string attIdentifier = "definition";
+
+            var e = Assert.Catch (() => {
+                ap.ParsedAttribute (attIdentifier,
+                                   null,
+                                   v);
+            });
+
+            Assert.IsInstanceOf (typeof (InvalidAttributeValueException), e);
+            StringAssert.AreEqualIgnoringCase (
+                string.Format (InvalidAttributeValueException.ATOMIC_ONLY, attIdentifier),
+                e.Message
+            );
+        }
+    }
+}
--- Parsing/Parsers/Attributes/DefinitionAttributeParser.cs
+++ Parsing/Parsers/Attributes/DefinitionAttributeParser.cs
@@ -1,6 +1,7 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using UCLouvain.KAOSTools.Parsing.Parsers.Exceptions;
 
 namespace KAOSTools.Parsing.Parsers.Attributes
 {
@@ -15,15 +16,18 @@ namespace KAOSTools.Parsing.Parsers.Attributes
 		public ParsedElement ParsedAttribute(string identifier, NParsedAttributeValue parameters, NParsedAttributeValue value)
         {
             if (parameters != null)
-				throw new NotImplementedException("Attribute 'definition' does not accept parameters.");
+                throw new InvalidParameterAttributeException (identifier,
+                                                              InvalidParameterAttributeException.NO_PARAM);
 
             if (!(value is NParsedAttributeAtomic))
-				throw new NotImplementedException("Attribute 'definition' only accept a single atomic value");
+                throw new InvalidAttributeValueException (identifier,
+                                                          InvalidAttributeValueException.ATOMIC_ONLY);
 
             var v = ((NParsedAttributeAtomic)value).Value;
 
             if (!(v is ParsedString))
-				throw new NotImplementedException("Attribute 'definition' only accept string value");
+                throw new InvalidAttributeValueException (identifier,
+                                                          InvalidAttributeValueException.STRING);
 
             return new ParsedDefinitionAttribute() { Value = ((ParsedString)((NParsedAttributeAtomic)value).Value) };
 		}
