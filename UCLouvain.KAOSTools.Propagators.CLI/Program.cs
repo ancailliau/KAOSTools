44a8aedf3b7928bc08d12862bf2f3abb5eb29fb8
Antoine Cailliau <antoine.cailliau@uclouvain.be>
2017-05-02 14:34:53 +0200

    Introduces a CLI for testing propagators

---
+++ UCLouvain.KAOSTools.Propagators.CLI/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UCLouvain.KAOSTools.Propagators.Benchmark;
+using System.Diagnostics;
+
+namespace UCLouvain.KAOSTools.Propagators.CLI
+{
+    class MainClass
+    {
+        public static void Main (string [] args)
+        {
+            Console.WriteLine ("Hello World!");
+
+            int max = 10;
+
+            var bench = new PatternVSBDD ();
+            bench.NbGoals = int.Parse (args [0]);
+            bench.NbObstructions = int.Parse (args [1]);
+            bench.NbObstacles = int.Parse (args [2]);
+            Console.Write ("Setup...");
+            bench.Setup ();
+            Console.WriteLine ("(done)");
+
+            Console.WriteLine ("Pattern-based computation:");
+            var watch = Stopwatch.StartNew ();
+            for (int i = 0; i < max; i++) {
+                bench.PatternBasedComputation ();
+            }
+            watch.Stop ();
+            Console.WriteLine ($"    Elapsed: {watch.ElapsedMilliseconds/max}ms");
+            
+            Console.WriteLine ("BDD-based computation:");
+            watch = Stopwatch.StartNew ();
+            for (int i = 0; i < max; i++) {
+                bench.BDDBasedComputation ();
+            }
+            watch.Stop ();
+            Console.WriteLine ($"    Elapsed: {watch.ElapsedMilliseconds/max}ms");
+            
+            Console.WriteLine ("BDD-based computation with a pre-built:");
+            watch = Stopwatch.StartNew ();
+            for (int i = 0; i < max; i++) {
+                bench.BDDBasedComputationPrebuilt ();
+            }
+            watch.Stop ();
+            Console.WriteLine ($"    Elapsed: {watch.ElapsedMilliseconds/max}ms");
+            
+        }
+    }
+}
