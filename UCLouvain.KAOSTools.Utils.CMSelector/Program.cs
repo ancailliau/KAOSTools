6b15d7f71d9895bd87ee4743052e4661093656b2
Antoine Cailliau <antoine.cailliau@uclouvain.be>
2017-06-22 10:42:47 +0200

    Introduces a new util to select most appropriate CM

---
+++ UCLouvain.KAOSTools.Utils.CMSelector/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using KAOSTools.Core;
+using KAOSTools.Utils;
+using UCLouvain.KAOSTools.Core.SatisfactionRates;
+using UCLouvain.KAOSTools.Propagators;
+using UCLouvain.KAOSTools.Propagators.BDD;
+
+namespace UCLouvain.KAOSTools.Utils.CMSelector
+{
+    class MainClass : KAOSToolCLI
+    {
+        public static void Main (string [] args)
+        {
+            Console.WriteLine ("*** This is CMSelector from KAOSTools. ***");
+            Console.WriteLine ("*** For more information on KAOSTools see <https://github.com/ancailliau/KAOSTools> ***");
+            Console.WriteLine ("*** Please report bugs to <https://github.com/ancailliau/KAOSTools/issues> ***");
+            Console.WriteLine ();
+            Console.WriteLine ("*** Copyright (c) 2017, Université catholique de Louvain ***");
+            Console.WriteLine ("");
+
+            string rootname = "root";
+            options.Add ("root=", "Specify the root goal for which the selection shall be optimized. (Default: root)", v => rootname = v);
+
+            bool bdd = true;
+            options.Add ("bdd", "Uses the BDD-Based propagation (Default)", v => bdd = true);
+            options.Add ("pattern", "Uses the Pattern-Based propagation", v => bdd = false);
+            
+            Init (args);
+            
+            var root = model.Goal (rootname);
+            if (root == null) {
+                PrintError ("The goal '"+rootname+"' was not found");
+            }
+
+            try {
+                
+            
+            } catch (Exception e) {
+                PrintError ("An error occured during the computation. ("+e.Message+").\n"
+                +"Please report this error to <https://github.com/ancailliau/KAOSTools/issues>.\n"
+                +"----------------------------\n"
+                +e.StackTrace
+                +"\n----------------------------\n");
+                
+            }
+        }
+    }
+}
